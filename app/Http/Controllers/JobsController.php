c569b73314408c484db9c6bda738b64d3ffa7900
Balram72 <balrampanda72@gmail.com>
2025-08-15 10:16:15 +0530

    using the search And filter Job

--- app/Http/Controllers/JobsController.php
+++ app/Http/Controllers/JobsController.php
@@ -10,13 +10,55 @@ use Illuminate\Http\Request;
 class JobsController extends Controller
 {
     // this methods will show the jobs page
-    public function index()
+    public function index(request $req)
     {
 
         $categories = Category::where('status', 1)->orderBy('name', 'asc')->get();
         $jobTypes = JobType::where('status', 1)->orderBy('name', 'asc')->get();
-        $Jobs = Job::where('status', 1)->orderBy('Created_at', 'DESC')
-            ->with('jobType')->paginate(9);
-        return view('front.jobs', compact('categories', 'jobTypes', 'Jobs'));
+
+        $Jobs = Job::where('status', 1);
+
+        // search using keywords
+        if (!empty($req->keyword)) {
+            $Jobs = $Jobs->where(function ($query) use ($req) {
+                $query->orWhere('title', 'like', '%' . $req->keyword . '%');
+                $query->orWhere('keywords', 'like', '%' . $req->keyword . '%');
+            });
+        }
+
+        //search using Location
+
+        if (!empty($req->location)) {
+            $Jobs = $Jobs->where('location', $req->location);
+        }
+
+        //search using category
+
+        if (!empty($req->category)) {
+            $Jobs = $Jobs->where('category_id', $req->category);
+        }
+
+        //search using job type
+        $jobTypeArray = [];
+        if (!empty($req->jobType)) {
+            $jobTypeArray = explode(',', $req->jobType);
+            $Jobs = $Jobs->whereIn('job_type_id', $jobTypeArray);
+        }
+
+        // search using the experience
+        if (!empty($req->experience)) {
+            $Jobs = $Jobs->where('experience', $req->experience);
+        }
+
+        $Jobs = $Jobs->with(['jobType']);
+        if ($req->sort == '0') {
+            $Jobs = $Jobs->orderBy('created_at', 'ASC');
+        } else {
+            $Jobs = $Jobs->orderBy('created_at', 'DESC');
+        }
+        $Jobs = $Jobs->paginate(9);
+
+
+        return view('front.jobs', compact('categories', 'jobTypes', 'Jobs', 'jobTypeArray'));
     }
 }
